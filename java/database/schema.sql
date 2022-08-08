03b671bd4ab4b6614d59ceabe1cdca1c235a114e
Colton Baumgard <csbaumgard@gmail.com>
2022-08-08 12:21:27 -0400

    commit

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,6 +10,18 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE profile (
+	profile_id SERIAL,
+	user_id int,
+	goal_calories int,
+	current_weight int NOT NULL,
+	desired_weight int NOT NULL,
+	age int,
+	height int,
+	CONSTRAINT PK_profile PRIMARY KEY (profile_id),
+	CONSTRAINT FK_profile_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
 CREATE TABLE foods (
 	food_id SERIAL,
 	profile_id int,
@@ -20,18 +32,6 @@ CREATE TABLE foods (
 	calories int NOT NULL,
 	CONSTRAINT PK_food PRIMARY KEY (food_id),
 	CONSTRAINT FK_food_profile_id FOREIGN KEY (profile_id) REFERENCES profile (profile_id)
-);
-
-CREATE TABLE profile (
-	profile_id SERIAL,
-	user_id int,
-	goal_calories int,
-	current_weight int NOT NULL,
-	desired_weight int NOT NULL,
-	age int,
-	height int,
-	CONSTRAINT PK_profile PROFILE KEY (profile_id),
-	CONSTRAINT FK_profile_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 COMMIT TRANSACTION;
