6a8e198bb47d19244b635bf6a2a9b2760861e542
brian bowling <bowling.duq@gmail.com>
2022-08-09 15:06:50 -0400

    styling and profile page

---
+++ vue/src/services/ProfileService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default{
+
+create(Profile profile){
+return axios.get('/profile')
+
+}
+} 
---
+++ vue/src/views/testview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <img :src="require('./assets/logo.png')" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
