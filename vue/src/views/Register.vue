9ef94f42c551f0c5638277b9425cfa9d8eafc6bb
brian bowling <bowling.duq@gmail.com>
2022-08-10 16:22:37 -0400

    more styling for demo

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="register" class="text-center">
     <form class="form-register" @submit.prevent="register">
+      <img src="https://prod.wp.cdn.aws.wfu.edu/sites/22/2020/04/iStock-854725402.blog_.jpg" width="500" />
+      <h1 class="h3 mb-3 font-weight-normal">Welcome to Waist Management</h1>
       <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
