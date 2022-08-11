ecb059c9e0f7482c24ef013c2a92b835fe3c984d
Colton Baumgard <csbaumgard@gmail.com>
2022-08-11 10:55:05 -0400

    commit

--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -4,7 +4,7 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'pickles';
-DROP DATABASE pickles;
+WHERE datname = 'final_capstone';
+DROP DATABASE final_capstone;
 DROP USER final_capstone_owner;
 DROP USER final_capstone_appuser;
