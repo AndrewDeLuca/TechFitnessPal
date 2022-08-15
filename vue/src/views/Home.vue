09c99c147b0172c14a594d28889afbe11fd06b06
brian bowling <bowling.duq@gmail.com>
2022-08-15 19:45:43 -0400

    new style for home page bb

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,39 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
     
+    <body>
+      <h1>Waist Management Home</h1>
+      <h2>Welcome to Waist Management!!!!</h2>
+      <h3>Are you ready to become the best version of yourself!!!!????</h3>
+      <img src=https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/7471e64bf36dd74cb4c52b2dec40690b-1605191302/5%20workout%20gif/create-workout-exercise-gif-animation-in-photoshop.gif>
+   
+   <h4>source: google images/fiverr</h4>
+   </body>
+   <div class= "div">
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+   </div>
+
   </div>
 </template>
 
@@ -11,3 +42,26 @@ export default {
   name: "home"
 };
 </script>
+
+<style scoped>
+img {
+
+  width: 250px;
+  height: 200px;
+  margin-bottom: 0%;
+  padding-bottom: 0%;
+}
+h3{
+  margin-bottom: 0%;
+}
+h4 {
+
+  margin-top: 0%;
+  font-style: italic;
+}
+
+.div{
+
+  background-image: url("https://prod.wp.cdn.aws.wfu.edu/sites/22/2020/04/iStock-854725402.blog_.jpg");
+}
+</style>
